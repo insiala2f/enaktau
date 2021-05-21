d8f80489eee5187272fa0b483c5baa73990326ae
insiala2f <84434303+insiala2f@users.noreply.github.com>
2021-05-21 17:42:21 +0800

    Update rafgtta.js

--- rafgtta.js
+++ rafgtta.js
@@ -1,7 +1,6 @@
 var dataUrl = [
   '//wonderfullytumultbizarre.com/k78nwxfv?key=e668079d92f5c385e97d9e9f30beedd3',
   '//wonderfullytumultbizarre.com/ezuq8wdg?key=abe65c5f1fac7c85a5e79772f3e4b4ab',
-  '//usounoul.com/4/4252725',
   '//wonderfullytumultbizarre.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
   '//wonderfullytumultbizarre.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e'
 ];
