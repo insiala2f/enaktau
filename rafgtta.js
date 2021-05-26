3e037fa09da2e89be1c29328626fb4d63dab584c
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-05-26 18:54:30 +0800

    Update rafgtta.js

--- rafgtta.js
+++ rafgtta.js
@@ -3,6 +3,9 @@ var dataUrl = [
   '//wonderfullytumultbizarre.com/ezuq8wdg?key=abe65c5f1fac7c85a5e79772f3e4b4ab',
   '//wonderfullytumultbizarre.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
   '//wonderfullytumultbizarre.com/mcq9dwrp?key=26877099e93b852a73d06043b22429bd',
+  '//wonderfullytumultbizarre.com/y425t3d5k?key=b0d195ca768aeab7d18aad598d71b0b0',
+  '//wonderfullytumultbizarre.com/nuudznpz3?key=3d998b3da5397f5ac865ff0734c2be19',
+  '//wonderfullytumultbizarre.com/y425t3d5k?key=b0d195ca768aeab7d18aad598d71b0b0',
   '//wonderfullytumultbizarre.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
