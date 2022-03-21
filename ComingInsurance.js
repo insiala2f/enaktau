6c5f3b34987a4ec47e5eb30ad6faa9ac91b65210
insiala2f <84434303+insiala2f@users.noreply.github.com>
2022-03-21 02:53:53 -0700

    Update ComingInsurance.js

--- ComingInsurance.js
+++ ComingInsurance.js
@@ -1,5 +1,5 @@
 var dataUrl = [
- '//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac'
+ '//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
 '//wavermerchandiseweird.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
 '//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
 '//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
